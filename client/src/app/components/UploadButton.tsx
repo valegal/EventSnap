dedb1e08808a70e6a5c821e585f92aca47158d63
Valentina Galeano <valentinagaleanow@gmail.com>
2025-03-19 14:08:26 -0500

    Update design

--- client/src/app/components/UploadButton.tsx
+++ client/src/app/components/UploadButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import UploadCloud from "../icons/UploadCloud";
+import UploadCloud from "../icons/UploadCloud"; // Asegúrate de que la ruta es correcta
 
 const UploadButton = () => {
   const [files, setFiles] = useState<FileList | null>(null);
@@ -13,13 +13,15 @@ const UploadButton = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen border-2 border-b-blue-600 border-dashed bg-gray-200 p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen border-3 border-blue-700 border-dashed bg-gray-50 p-6">
       <label
         htmlFor="file-upload"
-        className="cursor-pointer  hover:bg-gray-100 text-black text-xl sm:text-2xl font-bold py-6 px-12 rounded-2xl shadow-lg flex flex-col items-center gap-3 transition-all duration-300"
+        className="cursor-pointer bg-white text-blue-950 text-3xl sm:text-center text-center sm:text-2xl font-bold py-6 px-12 rounded-2xl border border-dashed border-blue-600 shadow-xl flex flex-col items-center gap-4 transition-all duration-300 transform hover:scale-105"
       >
-        <UploadCloud />
-        <span>Subir Fotos y Videos</span>
+        <div className="flex items-center justify-center w-24 h-24 bg-blue-100 rounded-full shadow-md">
+          <UploadCloud className="w-12 h-12 text-blue-700" />
+        </div>
+        <span>Subir fotos y videos aquí</span>
       </label>
       <input
         id="file-upload"
@@ -29,8 +31,8 @@ const UploadButton = () => {
         className="hidden"
         onChange={handleFileChange}
       />
-      <p className="text-gray-600 text-center mt-4 text-lg sm:text-xl">
-        Puedes subir hasta <strong>{maxPhotos} fotos</strong> y <strong>{maxVideos} videos</strong>.
+      <p className="text-gray-700 text-center font-mono mt-4 text-md sm:text-md">
+      <span className="text-blue-500">* </span> Puedes subir hasta <strong>{maxPhotos} fotos</strong> y <strong>{maxVideos} videos</strong>.
       </p>
     </div>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import UploadButton from "./components/UploadButton";
 
 export default function Home() {
